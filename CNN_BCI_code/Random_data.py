b563870ff330dd1073621e470532d1fa5bf28a4d
Seohyun Moon <57365389+ski1999@users.noreply.github.com>
2024-02-15 15:11:35 -0500

    Update Random_data.py
    Centralize parameters

--- CNN_BCI_code/Random_data.py
+++ CNN_BCI_code/Random_data.py
@@ -76,79 +76,81 @@ with open('random_list.csv', 'w', newline='') as f:
 """
 
 path = "/home/ski1999/CNN_pcc_noaug_fold"
+mode = "Hamiltonian" #"Random", "Functional_Random"
+aug_size = 30
+hem_flag = 'n' #Only for mode "Functional_Random"
+new_fold_name = "CNN_pcc_funcv5_fold" #fold name
 directory = os.listdir(path)
 os.chdir(path)
 
 #dist_list = [0,10,31,21,42,43,22,1,12,11,32,33,34,13,2,23,44,45,24,3,4,5,26,15,14,25,46,35,36,37,16,6,7,28,17,18,39,40,29,8,9,19,20,41,30,51,50,49,38,27,48,47]
 
 #Hamiltonian path
-for (path,dir,files) in os.walk(path):
-    for file in files:
-        new_path = os.path.join(path,file)
-        fileMatrix = []
-        datafile = []
-        with open(new_path,'r') as open_file:
-            read_file = csv.reader(open_file)
-            for line in read_file:
-                datafile.append(line)
-            #print(len(datafile))
-        for cnt in range(150):
-            aug_data = array_aug(datafile,dist_list[cnt])
-            new_filename = 'aug_'+str(cnt+1)+'_'+file
-            new_filename = os.path.join(path,new_filename)
-            new_filename_dir = new_filename.split("/")
-            new_filename_dir[3] = "CNN_plv_hamv1_fold"
-            new_filename = "/".join(new_filename_dir)
-            #print(new_filename)
-            with open(new_filename,'w',newline='') as f:
-                write = csv.writer(f)
-                write.writerows(aug_data)
+if mode == "Hamiltonian":
+    for (path,dir,files) in os.walk(path):
+        for file in files:
+            new_path = os.path.join(path,file)
+            fileMatrix = []
+            datafile = []
+            with open(new_path,'r') as open_file:
+                read_file = csv.reader(open_file)
+                for line in read_file:
+                    datafile.append(line)
+                #print(len(datafile))
+            for cnt in range(aug_size):
+                aug_data = array_aug(datafile,dist_list[cnt])
+                new_filename = 'aug_'+str(cnt+1)+'_'+file
+                new_filename = os.path.join(path,new_filename)
+                new_filename_dir = new_filename.split("/")
+                new_filename_dir[3] = new_fold_name
+                new_filename = "/".join(new_filename_dir)
+                #print(new_filename)
+                with open(new_filename,'w',newline='') as f:
+                    write = csv.writer(f)
+                    write.writerows(aug_data)
 
-"""
+
 #Random Path
-for (path,dir,files) in os.walk(path):
-    for file in files:
-        new_path = os.path.join(path,file)
-        datafile = pd.read_csv(new_path,header=None)
-        for cnt in range(150):
-            aug_data = shuffle_aug(datafile,cnt)
-            filename_list = file.split("_")
-            filename_list[1] = str(cnt+1)
-            new_filename = '_'.join(filename_list)
-            #new_filename = 'aug_'+str(cnt+1)+'_'+file
-            new_filename = os.path.join(path,new_filename)
-            #print(new_filename)
-            aug_data.to_csv(new_filename, index=False)
-"""
+elif mode = "Random":
+    for (path,dir,files) in os.walk(path):
+        for file in files:
+            new_path = os.path.join(path,file)
+            datafile = pd.read_csv(new_path,header=None)
+            for cnt in range(aug_size):
+                aug_data = shuffle_aug(datafile,cnt)
+                new_filename = 'aug_'+str(cnt+1)+'_'+file
+                new_filename = os.path.join(path,new_filename)
+                new_filename_dir = new_filename.split("/")
+                new_filename_dir[3] = new_fold_name
+                new_filename = "/".join(new_filename_dir)
+                #print(new_filename)
+                aug_data.to_csv(new_filename, index=False)
 
-"""
 #Functional Random Path (Region ordering Hamiltonian)
-hem_flag = 'n'
-if hem_flag == 'y':
-    region_list = region_list_y
-elif hem_flag == 'n':
-    region_list = region_list_n
-for (path,dir,files) in os.walk(path):
-    for file in files:
-        new_path = os.path.join(path,file)
-        datafile = pd.read_csv(new_path,header=None)
-        for cnt in range(150):
-            random.seed(cnt)
-            select_path = random.randint(0,len(region_list)-1)
-            #region_path = region_list[select_path][:]
-            #functional_list = function_shuffle_aug(cnt,hem_flag,region_path)
-            functional_list = function_shuffle_aug(cnt,hem_flag)
-            #print(len(functional_list))
-            #print(new_path)
-            aug_data = array_aug(datafile, functional_list)
-            new_filename = 'aug_'+str(cnt+1)+'_'+file
-            new_filename = os.path.join(path,new_filename)
-            new_filename_dir = new_filename.split("/")
-            new_filename_dir[3] = "CNN_pcc_funcv5_fold"
-            new_filename = "/".join(new_filename_dir)
-            #print(new_filename)
-            with open(new_filename,'w',newline='') as f:
-                write = csv.writer(f)
-                write.writerows(aug_data)
-"""
-                
+elif mode = "Functional_Random":
+    if hem_flag == 'y':
+        region_list = region_list_y
+    elif hem_flag == 'n':
+        region_list = region_list_n
+    for (path,dir,files) in os.walk(path):
+        for file in files:
+            new_path = os.path.join(path,file)
+            datafile = pd.read_csv(new_path,header=None)
+            for cnt in range(150):
+                random.seed(cnt)
+                select_path = random.randint(0,len(region_list)-1)
+                #region_path = region_list[select_path][:]
+                #functional_list = function_shuffle_aug(cnt,hem_flag,region_path)
+                functional_list = function_shuffle_aug(cnt,hem_flag)
+                #print(len(functional_list))
+                #print(new_path)
+                aug_data = array_aug(datafile, functional_list)
+                new_filename = 'aug_'+str(cnt+1)+'_'+file
+                new_filename = os.path.join(path,new_filename)
+                new_filename_dir = new_filename.split("/")
+                new_filename_dir[3] = new_fold_name
+                new_filename = "/".join(new_filename_dir)
+                #print(new_filename)
+                with open(new_filename,'w',newline='') as f:
+                    write = csv.writer(f)
+                    write.writerows(aug_data)
